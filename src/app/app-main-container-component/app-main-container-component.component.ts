b040fb3234fbb20adcf6ae7b18c9eb18582c1f45
markokeeffe <markokeeffe@vsware.ie>
2017-10-11 15:49:53 +0100

    Animation

--- src/app/app-main-container-component/app-main-container-component.component.ts
+++ src/app/app-main-container-component/app-main-container-component.component.ts
@@ -1,10 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-app-main-container-component',
   templateUrl: './app-main-container-component.component.html',
-  styleUrls: ['./app-main-container-component.component.css']
+  styleUrls: ['./app-main-container-component.component.css'],
+  animations: [
+    trigger('myAwesomeAnimation',[
+      state('small', style({
+        transform: 'scale(1)',
+      })),
+      state('large', style({
+        transform: 'scale(1.2)',
+      })),
+
+      transition('small <=> large', animate('300ms ease-in', keyframes([
+        style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+        style({opacity: 1, transform: 'translateY(35px)', offset: .5}),
+        style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+      ]))),
+    ]),
+
+  ]
 })
 export class AppMainContainerComponentComponent implements OnInit {
   buttonStatus='josephMaxey';
@@ -15,6 +33,12 @@ export class AppMainContainerComponentComponent implements OnInit {
 
   myMouseEnterEvent(event){
     console.log(event);
+  }
+
+  state: string = 'small;'
+
+  animateMe(){
+    this.state = (this.state === 'small' ? 'large' : 'small');
   }
 
   titleClasses={
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MyNewComponentComponent } from './my-new-component/my-new-component.com
 import { HeadComponentComponent } from './head-component/head-component.component';
 import { AppMainContainerComponentComponent } from './app-main-container-component/app-main-container-component.component';
 import { DataService } from './data.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { DataService } from './data.service';
     AppMainContainerComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
